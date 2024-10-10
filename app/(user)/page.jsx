798da471777f5c7faea46770180c9302c518df75
Furkan <furkancosar2005@gmail.com>
2024-10-10 16:53:56 +0300

    admin panele geçiş sağlandı

--- app/(user)/page.jsx
+++ app/(user)/page.jsx
@@ -1,6 +1,15 @@
 import { signOut } from "../login/acitons";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const supabase = createClient();
+  const {data: { user }} = await supabase.auth.getUser()
+
+  if(user?.user_metadata?.role === "admin") {
+    redirect("/admin")
+  }
+
   return (
     <div>
       <h1>user home page</h1>

5434e5b136783e75abd2094b25130f54b74b4635
muhammet hasan <muhammethasanturkmen@gmail.com>
2024-10-10 17:10:06 +0300

    ogrenciler bastırıldı

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -1,13 +1,27 @@
 import { signOut } from "@/app/login/actions";
+import { createClient } from "@/utils/supabase/server";
 
-export default async function AdminPage(){
-    return(
+export default async function AdminPage() {
+    const supabase = createClient();
+
+    let { data: students } = await supabase
+    .from('students')
+    .select('*')
+    console.log(students)
+    return (
         <div>
             <h1>admin sayfası</h1>
 
             <form action={signOut}>
                 <button>sign out</button>
             </form>
+
+            {students.map((x, i) =>
+                <div className="student" key={i}>
+                    <p>{x.firstName}</p>
+                    <p>{x.lastName}</p>
+                </div>
+            )}
         </div>
     )
 }

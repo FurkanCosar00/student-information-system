3dac2456d1f04c1d1787cf45900bf9d9add6bf18
Furkan <furkancosar2005@gmail.com>
2024-10-10 16:47:35 +0300

    signup ve login düzenlendi

--- app/login/acitons.js
+++ app/login/acitons.js
@@ -4,6 +4,41 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
+export async function signup(formData) {
+  const supabase = createClient()
+
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+    options: {
+      data: {
+        role: "student"
+      }
+    }
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  const { error: insertError } = await supabase
+  .from('students')
+  .insert([
+      { 
+        firstName: formData.get("firstName"),
+        lastName: formData.get("lastName"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+        role: "student"
+      }
+  ]);
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
 export async function login(formData) {
   const supabase = createClient()
 
@@ -13,24 +48,6 @@ export async function login(formData) {
   }
 
   const { error } = await supabase.auth.signInWithPassword(data)
-
-  if (error) {
-    redirect('/error')
-  }
-
-  revalidatePath('/', 'layout')
-  redirect('/')
-}
-
-export async function signup(formData) {
-  const supabase = createClient()
-
-  const data = {
-    email: formData.get('email'),
-    password: formData.get('password'),
-  }
-
-  const { error } = await supabase.auth.signUp(data)
 
   if (error) {
     redirect('/error')
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,13 +2,26 @@ import { login, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <>
+      <form className="signup">
+        <label htmlFor="firstName">firstname:</label>
+        <input id="firstName" name="firstName" type="firstName" required />
+        <label htmlFor="lastName">lastname:</label>
+        <input id="lastName" name="lastName" type="lastName" required />
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={signup}>Sign up</button>
+      </form>
+
+      <form className="login">
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={login}>Log in</button>
+      </form>
+    </>
   )
 }
